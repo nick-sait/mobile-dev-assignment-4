c523d4362133e4a295a76fd800a9c22dd62f2d6b
Nick <nickolas.tetzlaff@edu.sait.ca>
2026-03-03 10:49:21 -0700

    forgot one page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -9,6 +9,7 @@ export const unstable_settings = {
   anchor: '(tabs)',
 };
 
+
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
@@ -17,8 +18,6 @@ export default function RootLayout() {
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-        <Stack.Screen name="sign-up" options={{ headerShown: false}} />
-        <Stack.Screen name="login" options={{ headerShown: false}} />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
